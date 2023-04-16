c787c195de051e5accf5ed56d9f32b471d6deeda
jaisonmf <85539202+jaisonmf@users.noreply.github.com>
2023-04-17 09:54:04 +1000

    please dont work in my testing scene :)

---
+++ Leviticus/Assets/Scripts/Jaison Testing/iconStuff.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class iconStuff : MonoBehaviour
+{
+    [SerializeField] GameObject spawnedIcon;
+
+    /*
+    [SerializeField] Image fire;
+    [SerializeField] Image poison;
+    [SerializeField] Image stun;
+    [SerializeField] Image healOverTime;
+    [SerializeField] Image powerful;
+    */
+
+    [SerializeField] Image test;
+
+
+    [SerializeField] Image appliedEffect;
+
+
+    public void effectApplied()
+    {
+
+    }
+
+
+
+
+}
---
+++ Leviticus/Assets/Scripts/Jaison Testing/statusEffect.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class statusEffect : MonoBehaviour
+{
+    [SerializeField] Button fire;
+    [SerializeField] Button poison;
+    [SerializeField] Button Stun;
+    [SerializeField] Button healOverTime;
+    [SerializeField] Button powerful;
+
+
+}
